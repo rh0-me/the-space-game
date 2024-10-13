59b90e03eb89ef78be59ff92f9a76912d97c73d6
Roman <85564248+Roman-S-OS@users.noreply.github.com>
2024-10-13 11:07:44 +0200

    add: highlighting of selected buildings

--- TheSpaceGame/TheSpaceGame/the_space_game/src/app/components/game-bar/game-bar.component.ts
+++ TheSpaceGame/TheSpaceGame/the_space_game/src/app/components/game-bar/game-bar.component.ts
@@ -1,34 +1,50 @@
 import { Component } from '@angular/core';
 import { Buildings } from 'src/app/enums/buildings.enum';
 import { GameManagerService } from 'src/app/services/game-manager.service';
-import {Moment} from 'moment/moment';
+import { Moment } from 'moment/moment';
 import * as moment from 'moment/moment';
 
 @Component({
   selector: 'app-game-bar',
   templateUrl: './game-bar.component.html',
-  styleUrls: ['./game-bar.component.css']
+  styleUrls: ['./game-bar.component.css'],
 })
 export class GameBarComponent {
-
   Buildings = Buildings;
 
   private selectedBuildingType: Buildings | null = null;
+  private selectedBuildingElement: HTMLElement | null = null;
 
   constructor(private gameService: GameManagerService) {}
 
-  selectBuilding(buildingType: Buildings): void {
-    this.selectedBuildingType = buildingType;  // Setzt den aktuellen Gebäudetyp
+  selectBuilding(buildingType: Buildings, event: MouseEvent): void {
+    const target = (event.target as HTMLElement).closest(
+      '.building-btn'
+    ) as HTMLElement;
+    if (!target) {
+      return;
+    } // selects the respective button and considers the span or other elements inside the btn
+
+    // highlights the currently selected building
+    if (
+      this.selectedBuildingElement &&
+      this.selectedBuildingElement !== target
+    ) {
+      this.selectedBuildingElement.style.backgroundColor = '';
+      // reset of the previous building
+    }
+    target.style.backgroundColor = 'aquamarine';
+    this.selectedBuildingElement = target;
+    this.selectedBuildingType = buildingType; // Setzt den aktuellen Gebäudetyp
   }
 
-  #getTime(): Date {
-    return new Date(this.gameService.getRunningTime());
-  }
+  // #getTime(): Date {
+  //   return new Date(this.gameService.getRunningTime());
+  // }
 
   public getTimeString(): String {
-    let test: Date = this.#getTime();
-    
+    // let test: Date = this.#getTime();
+
     return moment().format('h:mm:ss a'); // October 7th 2024, 11:30:50 am
   }
-
 }
